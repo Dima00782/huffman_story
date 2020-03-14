d13d941555e820e2f7818289415f0c94318ab1cd
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2020-03-14 20:29:34 +0700

    next minute: switched to war_and_piece in perf-tests by default because now it runs pretty fast.

--- perf_tests/perf_tests.py
+++ perf_tests/perf_tests.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 start_time = time.time()
-process = subprocess.run(['main/main', 'crypt', 'perf_tests/lorem_ipsum.txt'], 
+process = subprocess.run(['main/main', 'crypt', 'perf_tests/war_and_peace.txt'], 
                          stdout=subprocess.PIPE, 
                          universal_newlines=True)
 end_time = time.time()
