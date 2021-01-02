55f6d8f9c73bec399cbc575eb52cc80e869ea1e3
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2021-01-02 18:26:08 +0700

    extract letter serializer

--- letter/letter.h
+++ letter/letter.h
@@ -18,18 +18,38 @@ namespace letter {
 class Letter {
  public:
   virtual ~Letter() {}
-  virtual std::unique_ptr<Letter> ReadSerialized(bit_io::BitReader& input) = 0;
-  virtual void WriteSerialized(bit_io::BitWriter& output) = 0;
   virtual std::size_t Hash() const noexcept = 0;
 
   // TODO: REMOVE ME.
   virtual std::string toString() const = 0;
 };
 
+class LetterSerializer {
+ public:
+  virtual ~LetterSerializer() {}
+  virtual std::unique_ptr<Letter> ReadSerialized(bit_io::BitReader& input) = 0;
+  virtual bool WriteSerialized(bit_io::BitWriter& output, const Letter& letter) = 0;
+};
+
 class LetterLexer {
  public:
   virtual ~LetterLexer() {}
-  virtual std::vector<std::unique_ptr<Letter>> Split(std::shared_ptr<std::istream> input) = 0;
+  virtual std::vector<std::unique_ptr<Letter>> Split(
+      std::shared_ptr<std::istream> input) = 0;
+};
+
+class LetterConfig {
+ public:
+  LetterConfig(std::unique_ptr<LetterLexer> lexer,
+               std::unique_ptr<LetterSerializer> serializer)
+      : lexer_(std::move(lexer)), serializer_(std::move(serializer)) {}
+
+  LetterLexer& Lexer();
+  LetterSerializer& Serializer();
+
+ private:
+  std::unique_ptr<LetterLexer> lexer_;
+  std::unique_ptr<LetterSerializer> serializer_;
 };
 
 }  // namespace letter
--- letter/one_byte_letter.h
+++ letter/one_byte_letter.h
@@ -18,18 +18,6 @@ class OneByteLetter final : public Letter {
   explicit OneByteLetter(std::byte content) : content_{content} {}
   ~OneByteLetter() override {}
 
-  std::unique_ptr<Letter> ReadSerialized(bit_io::BitReader& input) override {
-    const auto byte = input.ReadByte();
-    if (byte) {
-      return std::make_unique<OneByteLetter>(static_cast<std::byte>(*byte));
-    }
-    return nullptr;
-  }
-
-  void WriteSerialized(bit_io::BitWriter& output) override {
-    output.WriteByte(static_cast<char>(content_));
-  }
-
   std::size_t Hash() const noexcept override {
     return std::hash<char>{}(static_cast<char>(content_));
   }
@@ -46,10 +34,12 @@ class OneByteLetterLexer final : public LetterLexer {
  public:
   ~OneByteLetterLexer() override {}
 
-  std::vector<std::unique_ptr<Letter>> Split(std::shared_ptr<std::istream> input) override {
+  std::vector<std::unique_ptr<Letter>> Split(
+      std::shared_ptr<std::istream> input) override {
     std::vector<std::unique_ptr<Letter>> letters;
     for (char symbol = '\0'; *input_ >> symbol;) {
-      letters.push_back(std::make_unique<OneByteLetter>(static_cast<std::byte>(symbol)));
+      letters.push_back(
+          std::make_unique<OneByteLetter>(static_cast<std::byte>(symbol)));
     }
     return letters;
   }
@@ -58,6 +48,26 @@ class OneByteLetterLexer final : public LetterLexer {
   std::shared_ptr<std::istream> input_;
 };
 
-}  // namespace symbol
+class OneByteLetterSerializer final : public LetterSerializer {
+ public:
+  ~OneByteLetterSerializer() override {}
+
+  std::unique_ptr<Letter> ReadSerialized(bit_io::BitReader& input) override {
+    const auto byte = input.ReadByte();
+    if (byte) {
+      return std::make_unique<OneByteLetter>(static_cast<std::byte>(*byte));
+    }
+    return nullptr;
+  }
+
+  bool WriteSerialized(bit_io::BitWriter& output, const Letter& letter) override {
+    assert(letter.toString().size() == 1);
+    output.WriteByte(static_cast<char>(letter.toString().front()));
+    // TODO: FIXME.
+    return true;
+  }
+};
+
+}  // namespace letter
 
 #endif  // SYMBOL_ONE_BYTE_SYMBOL_H_
