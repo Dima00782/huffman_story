bbc7cf54b47d147bd82e48dbd03b11e953847a8f
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2021-02-25 20:56:03 +0700

    remove usage of lexer from tree's tests

--- test/huffman_tree_builder_unittest.cc
+++ test/huffman_tree_builder_unittest.cc
@@ -1,5 +1,4 @@
 #include <memory>
-#include <string>
 
 #include "encoding/huffman_tree/huffman_tree_builder.h"
 #include "gtest/gtest.h"
@@ -9,47 +8,44 @@ namespace huffman_tree {
 
 class HuffmanTreeBuilderTest : public ::testing::Test {
  public:
-  std::unique_ptr<TreeNode<std::byte>> BuildTree(const std::string& text) {
-    std::istringstream string_input(text);
-    // TODO: remove lexer from here!
-    letter::ByteLetterLexer lexer;
-    return BuildHuffmanTree<std::byte, std::vector<std::byte>>(
-        lexer.Split(string_input));
+  std::unique_ptr<TreeNode<char>> BuildTree(
+      const std::vector<char>& parsed_text) {
+    return BuildHuffmanTree<char, std::vector<char>>(parsed_text);
   }
 };
 
 TEST_F(HuffmanTreeBuilderTest, OneSymbol) {
-  const std::string kTestString = "aaaa";
-  const auto root = BuildTree(kTestString);
+  const std::vector<char> parsed_text = {'a', 'a', 'a', 'a'};
+  const auto root = BuildTree(parsed_text);
 
   ASSERT_TRUE(root);
   EXPECT_TRUE(root->isLeaf());
-  EXPECT_EQ(root->key_, static_cast<std::byte>('a'));
+  EXPECT_EQ(root->key_, 'a');
   EXPECT_EQ(root->frequency_, 4u);
 }
 
 TEST_F(HuffmanTreeBuilderTest, TwoSymbols) {
-  const std::string kTestString = "aaaabbb";
-  const auto root = BuildTree(kTestString);
+  const std::vector<char> parsed_text = {'a', 'a', 'a', 'a', 'b', 'b', 'b'};
+  const auto root = BuildTree(parsed_text);
 
   ASSERT_TRUE(root);
   EXPECT_TRUE(root->isInner());
   EXPECT_EQ(root->frequency_, 7u);
 
   auto* left = root->left_.get();
-  EXPECT_EQ(left->key_, static_cast<std::byte>('b'));
+  EXPECT_EQ(left->key_, 'b');
   EXPECT_EQ(left->frequency_, 3u);
   EXPECT_TRUE(left->isLeaf());
 
   auto* right = root->right_.get();
-  EXPECT_EQ(right->key_, static_cast<std::byte>('a'));
+  EXPECT_EQ(right->key_, 'a');
   EXPECT_EQ(right->frequency_, 4u);
   EXPECT_TRUE(right->isLeaf());
 }
 
 TEST_F(HuffmanTreeBuilderTest, ThreeSymbols) {
-  const std::string kTestString = "aaaabbc";
-  const auto root = BuildTree(kTestString);
+  const std::vector<char> parsed_text = {'a', 'a', 'a', 'a', 'b', 'b', 'c'};
+  const auto root = BuildTree(parsed_text);
 
   ASSERT_TRUE(root);
   EXPECT_TRUE(root->isInner());
@@ -60,18 +56,18 @@ TEST_F(HuffmanTreeBuilderTest, ThreeSymbols) {
   EXPECT_EQ(left->frequency_, 3u);
 
   auto* right = root->right_.get();
-  EXPECT_EQ(right->key_, static_cast<std::byte>('a'));
+  EXPECT_EQ(right->key_, 'a');
   EXPECT_EQ(right->frequency_, 4u);
   EXPECT_TRUE(right->isLeaf());
 
   auto* left_left = left->left_.get();
   EXPECT_TRUE(left_left->isLeaf());
-  EXPECT_EQ(left_left->key_, static_cast<std::byte>('c'));
+  EXPECT_EQ(left_left->key_, 'c');
   EXPECT_EQ(left_left->frequency_, 1u);
 
   auto* left_right = left->right_.get();
   EXPECT_TRUE(left_right->isLeaf());
-  EXPECT_EQ(left_right->key_, static_cast<std::byte>('b'));
+  EXPECT_EQ(left_right->key_, 'b');
   EXPECT_EQ(left_right->frequency_, 2u);
 }
 
